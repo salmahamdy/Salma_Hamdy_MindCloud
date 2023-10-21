e5b94cc34338fa83cca5b980929b1682d0166771
Salma Hamdy <88855692+salmahamdy@users.noreply.github.com>
2023-10-21 21:34:22 +0300

    Additional (#2)
    * Added my city
    
    * Added comment
    
    * Added fav color

--- SalmaHamdy.py
+++ SalmaHamdy.py
@@ -4,4 +4,7 @@ department = "Communication and electronics"
 GraduationYear=2025
 myfavteam = "software team"
 myschool = "EGC"
+//geographic info
 Country="EGYPT"
+City = "Alexandria"
+favcolor="ligthblue"
